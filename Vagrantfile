7742b3a22bb92f8352334207b7fd6562213d4764
igorcoding <igorcoding@gmail.com>
2015-03-13 15:18:04 +0300

    Added my Vagrantfile with provision

---
+++ Vagrantfile
@@ -0,0 +1,23 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+  config.vm.box = "ubuntu/precise64"
+
+  config.vm.synced_folder "./", "/home/vagrant/EV-Tarantool1.6/"
+
+  config.vm.provision "shell",
+    privileged: false,
+    path: "provision/run.sh"
+
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 1600
+    v.cpus = 2
+    v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+    v.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+  end
+
+end
---
+++ provision/run.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+wget -q -O - http://tarantool.org/dist/public.key | sudo apt-key add -
+release=`lsb_release -c -s`
+
+sudo bash -c 'cat > /etc/apt/sources.list.d/tarantool.list <<- EOF
+deb http://tarantool.org/dist/master/ubuntu/ `lsb_release -c -s` main
+deb-src http://tarantool.org/dist/master/ubuntu/ `lsb_release -c -s` main
+EOF'
+
+sudo apt-get update
+sudo apt-get install -y tarantool
+
+USR_SRC=/usr/local/src
+wget http://c-ares.haxx.se/download/c-ares-1.9.1.tar.gz -O - | sudo tar -C ${USR_SRC} -xzvf -
+cd ${USR_SRC}/c-ares-1.9.1
+
+sudo ./configure
+sudo make
+sudo make install
+
+cd -
+
+curl -L https://cpanmin.us | sudo perl - App::cpanminus
+sudo cpanm Types::Serialiser
+sudo cpanm EV
+sudo cpanm AnyEvent
+
+if [ ${TRAVIS} == true ]; then
+	echo "TRAVIS"
+else
+    sudo apt-get install -y \
+        build-essential \
+        libcurl4-openssl-dev \
+        tarantool
+fi
+
+sudo service tarantool restart
