4d2860d3d117867bb2931c73952382dbcdd2fb74
igorcoding <igorcoding@gmail.com>
2016-04-04 19:16:11 +0300

    Changed tarantool distribution location

--- provision/tnt-cares.sh
+++ provision/tnt-cares.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
-
-wget -q -O - http://tarantool.org/dist/public.key | sudo apt-key add -
+sudo apt-get install -y curl
+curl https://packagecloud.io/tarantool/1_6/gpgkey | sudo apt-key add -
+sudo apt-get update
 release=`lsb_release -c -s`
 
 sudo bash -c 'cat > /etc/apt/sources.list.d/tarantool.list <<- EOF
-deb http://tarantool.org/dist/1.6/ubuntu/ `lsb_release -c -s` main
-deb-src http://tarantool.org/dist/1.6/ubuntu/ `lsb_release -c -s` main
+deb https://packagecloud.io/tarantool/1_6/ubuntu/ `lsb_release -c -s` main
+deb-src https://packagecloud.io/tarantool/1_6/ubuntu/ `lsb_release -c -s` main
 EOF'
 
 sudo apt-get update
 sudo apt-get install -y tarantool libexpat1-dev
 
 USR_SRC=/usr/local/src
-wget http://c-ares.haxx.se/download/c-ares-1.9.1.tar.gz -O - | sudo tar -C ${USR_SRC} -xzvf -
-cd ${USR_SRC}/c-ares-1.9.1
+wget http://c-ares.haxx.se/download/c-ares-1.10.0.tar.gz -O - | sudo tar -C ${USR_SRC} -xzvf -
+cd ${USR_SRC}/c-ares-1.10.0
 
 sudo ./configure
 sudo make
