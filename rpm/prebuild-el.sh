7a00f727c6b968933ecc4239d11e5d967af8297a
igorcoding <igorcoding@gmail.com>
2018-03-25 20:42:40 +0300

    fix

--- rpm/prebuild-el.sh
+++ rpm/prebuild-el.sh
@@ -7,6 +7,8 @@ curl -L http://cpanmin.us | perl - --sudo App::cpanminus
 echo "Installing Test::Tarantool"
 TestTarantool_VER=0.033
 TestTarantool_URL=https://github.com/igorcoding/Test-Tarantool16/releases/download/v${TestTarantool_VER}/Test-Tarantool16-${TestTarantool_VER}.tar.gz
-cpanm --sudo --notest ${TestTarantool_URL}
+TestTarantool_LOCATION=/tmp/test-tarantool16.tar.gz
+wget ${TestTarantool_URL} -O ${TestTarantool_LOCATION}
+cpanm --sudo ${TestTarantool_LOCATION}
 
 echo "Prebuild finished"
